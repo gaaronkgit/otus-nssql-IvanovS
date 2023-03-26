7fccf874bed971d49d2f88920e652a628343dd7e
gaaronkgit <gaaronk8325@gmail.com>
2023-03-26 18:43:16 +0500

    Add Idx, CK
    Добавил Создание индексов и ограничений

---
+++ HW14/HW_14_CreateCK.sql
@@ -0,0 +1,7 @@
+﻿/*Больше ограничений продумать не удалось :-)*/
+
+ALTER TABLE orderitem
+  ADD CONSTRAINT CK_Price_Greater_Than_Zero CHECK (price > 0);
+
+ALTER TABLE orderitem
+  ADD CONSTRAINT CK_Qu_Greater_Than_Zero CHECK (qu > 0);
---
+++ HW14/HW_14_CreateIdx.sql
@@ -0,0 +1,42 @@
+﻿USE prodsite
+GO
+
+CREATE CLUSTERED INDEX IDX_producttype_idproducttype
+ON prodsite.dbo.producttype (idproducttype)
+GO
+
+CREATE CLUSTERED INDEX IDX_productiontype
+ON prodsite.dbo.productiontype (idproductiontype, idproducttype)
+GO
+
+CREATE CLUSTERED INDEX IDX_people_idpeople
+ON prodsite.dbo.people (idpeople)
+GO
+
+CREATE CLUSTERED INDEX IDX_orders
+ON prodsite.dbo.orders (idorder, idcustomer)
+GO
+
+CREATE CLUSTERED INDEX IDX_orderitem
+ON prodsite.dbo.orderitem (idorder, idorderitem)
+GO
+
+CREATE CLUSTERED INDEX IDX_manufactdocpos
+ON prodsite.dbo.manufactdocpos (idmnufactdoc, idmanufactdocpos)
+GO
+
+CREATE CLUSTERED INDEX IDX_manufactdoc_idmanufactdoc
+ON prodsite.dbo.manufactdoc (idmanufactdoc)
+GO
+
+CREATE CLUSTERED INDEX IDX_docstate_iddocstate
+ON prodsite.dbo.docstate (iddocstate)
+GO
+
+CREATE CLUSTERED INDEX IDX_customertype_idcustomertype
+ON prodsite.dbo.customertype (idcustomertype)
+GO
+
+CREATE CLUSTERED INDEX IDX_customer_idcustomer
+ON prodsite.dbo.customer (idcustomer)
+GO
