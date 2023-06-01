03096514603366475d3bd1acdf89d9d17676255d
gaaronkgit <gaaronk8325@gmail.com>
2023-06-01 21:35:08 +0500

    Add HW16

---
+++ HW16/orderitem_idx.sql
@@ -0,0 +1,59 @@
+﻿CREATE INDEX idx_orderitem_idcolorin
+ON prodsite.dbo.orderitem (idcolorin)
+WITH (STATISTICS_NORECOMPUTE = ON)
+ON [PRIMARY]
+GO
+
+CREATE INDEX idx_orderitem_idcolorout
+ON prodsite.dbo.orderitem (idcolorout)
+WITH (STATISTICS_NORECOMPUTE = ON)
+ON [PRIMARY]
+GO
+
+CREATE INDEX idx_orderitem_idgood
+ON prodsite.dbo.orderitem (idgood)
+WITH (STATISTICS_NORECOMPUTE = ON)
+ON [PRIMARY]
+GO
+
+CREATE INDEX idx_orderitem_idmodel
+ON prodsite.dbo.orderitem (idmodel)
+WITH (STATISTICS_NORECOMPUTE = ON)
+ON [PRIMARY]
+GO
+
+CREATE INDEX idx_orderitem_idorder
+ON prodsite.dbo.orderitem (idorder)
+WITH (STATISTICS_NORECOMPUTE = ON)
+ON [PRIMARY]
+GO
+
+CREATE INDEX idx_orderitem_idpower
+ON prodsite.dbo.orderitem (idpower)
+WITH (STATISTICS_NORECOMPUTE = ON)
+ON [PRIMARY]
+GO
+
+CREATE INDEX idx_orderitem_idproductiontype
+ON prodsite.dbo.orderitem (idproductiontype)
+WITH (STATISTICS_NORECOMPUTE = ON)
+ON [PRIMARY]
+GO
+
+CREATE INDEX idx_orderitem_idversion
+ON prodsite.dbo.orderitem (idversion)
+WITH (STATISTICS_NORECOMPUTE = ON)
+ON [PRIMARY]
+GO
+
+CREATE INDEX idx_orderitem_parentid
+ON prodsite.dbo.orderitem (parentid)
+WITH (STATISTICS_NORECOMPUTE = ON)
+ON [PRIMARY]
+GO
+
+CREATE INDEX idx_orderitem_width
+ON prodsite.dbo.orderitem (width)
+WITH (STATISTICS_NORECOMPUTE = ON)
+ON [PRIMARY]
+GO
---
+++ HW16/orders_idx.sql
@@ -0,0 +1,83 @@
+﻿CREATE INDEX idx_orders_idaddress
+ON prodsite.dbo.orders (idaddress)
+WITH (STATISTICS_NORECOMPUTE = ON)
+ON [PRIMARY]
+GO
+
+CREATE INDEX idx_orders_idagree
+ON prodsite.dbo.orders (idagree)
+WITH (STATISTICS_NORECOMPUTE = ON)
+ON [PRIMARY]
+GO
+
+CREATE INDEX idx_orders_idagreement
+ON prodsite.dbo.orders (idagreement)
+WITH (STATISTICS_NORECOMPUTE = ON)
+ON [PRIMARY]
+GO
+
+CREATE INDEX idx_orders_idcustomer
+ON prodsite.dbo.orders (idcustomer)
+WITH (STATISTICS_NORECOMPUTE = ON)
+ON [PRIMARY]
+GO
+
+CREATE INDEX idx_orders_iddepartment
+ON prodsite.dbo.orders (iddepartment)
+WITH (STATISTICS_NORECOMPUTE = ON)
+ON [PRIMARY]
+GO
+
+CREATE INDEX idx_orders_iddestanation
+ON prodsite.dbo.orders (iddestanation)
+WITH (STATISTICS_NORECOMPUTE = ON)
+ON [PRIMARY]
+GO
+
+CREATE INDEX idx_orders_iddocoper
+ON prodsite.dbo.orders (iddocoper)
+WITH (STATISTICS_NORECOMPUTE = ON)
+ON [PRIMARY]
+GO
+
+CREATE INDEX idx_orders_iddocstate
+ON prodsite.dbo.orders (iddocstate)
+WITH (STATISTICS_NORECOMPUTE = ON)
+ON [PRIMARY]
+GO
+
+CREATE INDEX idx_orders_idordersgroup
+ON prodsite.dbo.orders (idordersgroup)
+WITH (STATISTICS_NORECOMPUTE = ON)
+ON [PRIMARY]
+GO
+
+CREATE INDEX idx_orders_idpeople
+ON prodsite.dbo.orders (idpeople)
+WITH (STATISTICS_NORECOMPUTE = ON)
+ON [PRIMARY]
+GO
+
+CREATE INDEX idx_orders_idseller
+ON prodsite.dbo.orders (idseller)
+WITH (STATISTICS_NORECOMPUTE = ON)
+ON [PRIMARY]
+GO
+
+CREATE INDEX idx_orders_idvalut
+ON prodsite.dbo.orders (idvalut)
+WITH (STATISTICS_NORECOMPUTE = ON)
+ON [PRIMARY]
+GO
+
+CREATE INDEX idx_orders_idversion
+ON prodsite.dbo.orders (idversion)
+WITH (STATISTICS_NORECOMPUTE = ON)
+ON [PRIMARY]
+GO
+
+CREATE INDEX ind_orders_idparent
+ON prodsite.dbo.orders (idparent)
+WITH (STATISTICS_NORECOMPUTE = ON)
+ON [PRIMARY]
+GO
